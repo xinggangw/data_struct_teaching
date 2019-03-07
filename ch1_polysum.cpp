82d60f3b5886728e0866c2d5cdaaa26235e3c13e
Xinggang Wang <xinggangw@qq.com>
2019-03-07 19:20:47 +0800

    new polysum

--- ch1_polysum.cpp
+++ ch1_polysum.cpp
@@ -2,11 +2,12 @@
 #include <math.h>
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-double f1(int n, double a[], double x)
+float f1(int n, float a[], float x)
 {
     int i;
-    double p = a[0];
+    float p = a[0];
     for (i=0; i<=n; ++i)
     {
         p += ( a[i] * pow(x,i) );
@@ -14,10 +15,10 @@ double f1(int n, double a[], double x)
     return p;
 }
 
-double f2(int n, double a[], double x)
+float f2(int n, float a[], float x)
 {
     int i;
-    double p = a[n];
+    float p = a[n];
     for (i=n; i>0; --i)
     {
         p = x*p + a[i-1];
@@ -28,22 +29,25 @@ double f2(int n, double a[], double x)
 
 int main()
 {
-    double a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-    int n = 9;
-    double x = 20.16;
+    int n = 100000;
+    float* a = (float *) malloc(sizeof(float)*(n+1));
+    // init a
+    for (int j=0; j<=n; ++j)
+        a[j] = (float)j*0.01;
+    float x = 1.0001;
     
     time_t begin, end;
-    double spent1, spent2;
+    float spent1, spent2;
     
     begin = clock();
-    double r1 = f1(n, a, x);
+    float r1 = f1(n, a, x);
     end = clock();
-    spent1 = (double)(end-begin) / CLOCKS_PER_SEC;
+    spent1 = (float)(end-begin) / CLOCKS_PER_SEC;
     
     begin = clock();
-    double r2 = f2(n, a, x);
+    float r2 = f2(n, a, x);
     end = clock();
-    spent2 = (double)(end-begin) / CLOCKS_PER_SEC;
+    spent2 = (float)(end-begin) / CLOCKS_PER_SEC;
     
     
     printf("result1 = %f\n", r1);
@@ -52,6 +56,7 @@ int main()
     printf("time spent1 = %f\n", spent1);
     printf("time spent2 = %f\n", spent2);
     
+    free(a);
     return 0;
 }
 
